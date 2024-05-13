85329af35c84759a48f0e304a3ca96a6576f65ad
Aatish Azad <aatishaz@amazon.com>
2024-05-13 23:22:06 +0530

    Added coments for Post Controller

--- src/main/java/com/intuit/comments/controller/PostController.java
+++ src/main/java/com/intuit/comments/controller/PostController.java
@@ -17,6 +17,10 @@ import com.intuit.comments.dto.PostDTO;
 import com.intuit.comments.entity.Post;
 import com.intuit.comments.service.PostService;
 
+/**
+ * The PostController class handles web requests to manage posts. It provides
+ * methods to add new posts and retrieve posts by a specific user.
+ */
 @RestController
 @RequestMapping("/api")
 public class PostController {
@@ -24,11 +28,25 @@ public class PostController {
 	@Autowired
 	private PostService postService;
 
+	/**
+	 * Creates a new post based on the data received in the PostDTO.
+	 * 
+	 * @param postDTO the DTO containing post data from the client
+	 * @return ResponseEntity with HTTP status 201 and created post data
+	 */
 	@PostMapping("/post")
 	public ResponseEntity<?> addPost(@RequestBody PostDTO postDTO) {
 		return ResponseEntity.status(201).body(postService.createPost(postDTO));
 	}
 
+	/**
+	 * Retrieves a list of posts made by a specific user, with pagination.
+	 * 
+	 * @param userId     the ID of the user whose posts are to be retrieved
+	 * @param pageNumber the page number to retrieve (zero-based)
+	 * @param pageSize   the number of posts per page
+	 * @return ResponseEntity containing the list of posts
+	 */
 	@GetMapping("/get/posts/by-user")
 	public ResponseEntity<?> getPostByUser(@RequestParam("userId") Long userId,
 			@RequestParam(value = "pageNumber", defaultValue = "0") Integer pageNumber,
